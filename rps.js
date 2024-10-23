08544f0ad59e2f477bbc29efb1cedd8daf5823a0
Leah505 <leah.clarke@ada.ac.uk>
2024-10-23 20:44:28 +0000

    Big bang version

--- rps.js
+++ rps.js
@@ -3,11 +3,15 @@ function rockPaperScissors(player1, player2) {
       return "draw";
   }
 
-  if (player1 === "rock" && player2 === "scissors") {
+  if (player1 === "rock" && (player2 === "scissors" || player2 === "lizard")) {
       return "player1";
-  } else if (player1 === "scissors" && player2 === "paper") {
+  } else if (player1 === "scissors" && (player2 === "paper" || player2 === "lizard")) {
       return "player1";
-  } else if (player1 === "paper" && player2 === "rock") {
+  } else if (player1 === "paper" && (player2 === "rock" || player2 === "spock")) {
+      return "player1";
+  } else if (player1 === "lizard" && (player2 === "spock" || player2 === "paper")) {
+      return "player1";
+  } else if (player1 === "spock" && (player2 === "scissors" || player2 === "rock")) {
       return "player1";
   }
 
