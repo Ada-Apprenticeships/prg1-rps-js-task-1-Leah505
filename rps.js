887b569fc5af46fbdfb9f804563ecb9bbadfa7d1
Leah505 <leah.clarke@ada.ac.uk>
2024-10-23 20:34:07 +0000

    First try

--- rps.js
+++ rps.js
@@ -1,19 +1,20 @@
 function rockPaperScissors(player1, player2) {
+  if (player1 === player2) {
+      return "draw";
+  }
 
-  
-   
+  if (player1 === "rock" && player2 === "scissors") {
+      return "player1";
+  } else if (player1 === "scissors" && player2 === "paper") {
+      return "player1";
+  } else if (player1 === "paper" && player2 === "rock") {
+      return "player1";
+  }
+
+  return "player2";
 }
 
-
-
-
-
-
-
-
-
-
-// Leave this code here for the automated tests
+// Code for the automated tests
 module.exports = {
   rockPaperScissors,
 }
